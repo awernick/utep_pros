594c5a89eb062f1c0ec248c5e719f98ccdf64de8
Guillermo Vargas <gavargas22@gmail.com>
2014-12-10 13:02:04 -0800

    Made the event element link to its view.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,19 @@
 class Event < ActiveRecord::Base
+
+	# Method for parsing the date to a readable format.
+	# Require Ruby's date library
+	require 'date'
+	def parse_date (unparsedDate)
+
+		# Convert date to string so that the method can work.
+		parsedDate = Date.parse(unparsedDate.to_s)
+
+		# Separate the date elements to display them with commas and space.
+		month = parsedDate.strftime('%B')
+		day = parsedDate.strftime('%d')
+		year = parsedDate.strftime('%Y')
+		# Show the date as we want it.
+		return month + " " + day + ", " + year
+	end
+
 end
