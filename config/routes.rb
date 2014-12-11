5f26303c593a35e1332d9b1ced9b9ce141aa57da
Guillermo Vargas <gavargas22@gmail.com>
2014-12-10 17:47:18 -0800

    Added more static pages, About and Others

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   resources :events
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  match '/contact' => 'static_pages#contact', via: [:get, :post]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
