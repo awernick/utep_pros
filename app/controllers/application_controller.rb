01b4204e5feda46433deab589316554c3de55bf1
Alan Wernick <awernick@miners.utep.edu>
2014-12-10 21:59:55 -0700

    Second SSO Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	require 'savon'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,7 +7,21 @@ class ApplicationController < ActionController::Base
   private
   
   def current_user
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id] else user_from_sso
+  end
+
+  def user_from_sso
+		utep_cookie = cookies[:UTEP_SE]
+		utep_salt = cookies[:UTEP_SA]
+
+		if utep_cookie && utep_salt
+			client = Savon.client(wsdl: 'http://websvs.utep.edu/databaseservices/public/ExternalSignon.asmx?wsdl')
+			response = client.call(:get_user_by_ssiu, message: { sessionId: utep_cookie, salt: utep_salt, ipAdress: request.host, userAgent: request.user_agent})
+			user.name = response.body[:get_user_by_ssiu_response][:FullName]
+			user.username = response.body[:get_user_by_ssiu_response][:UserName]
+			user.email = response.body[:get_user_by_ssiu_response][:EmailAddress]
+			user.save!
+		end
   end
 
   helper_method :current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,5 @@
 class User < ActiveRecord::Base
 
-	require 'savon'
-
 	self.table_name = "atw_users"
 
-
-	def self.from_sso
-		utep_cookie = cookies[:UTEP_SE]
-		utep_salt = cookies[:UTEP_SA]
-
-		if utep_cookie? && utep_salt?
-			client = Savon.client(wsdl: 'http://websvs.utep.edu/databaseservices/public/ExternalSignon.asmx?wsdl')
-			response = client.call(:get_user_by_ssiu, message: { sessionId: utep_cookie, salt: utep_salt, ipAdress: request.host, userAgent: request.user_agent})
-			user.name = response.body[:get_user_by_ssiu_response][:FullName]
-			user.username = response.body[:get_user_by_ssiu_response][:UserName]
-			user.email = response.body[:get_user_by_ssiu_response][:EmailAddress]
-			user.save!
-		end
-	end
 end
