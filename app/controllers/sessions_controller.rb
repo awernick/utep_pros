cafa616a55097eeb395a313e14977c67f0fee446
Alan Wernick <awernick@miners.utep.edu>
2014-12-11 14:49:39 -0700

    Modify SSO structure to support sing-out test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,10 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		utep_cookie = cookies[:UTEP_SE]
 		session[:user_id] = nil
+		client = Savon.client(wsdl: 'http://websvs.utep.edu/databaseservices/public/ExternalSignon.asmx?wsdl')
+		client.call(:log_off, message: { sessionId: utep_cookie.to_s})
 		redirect_to root_url
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
 
   resources :events
 
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  match 'auth/:provider', to: 'sessions#create', via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   root 'static_pages#home'
   match '/contact' => 'static_pages#contact', via: [:get, :post]
 
