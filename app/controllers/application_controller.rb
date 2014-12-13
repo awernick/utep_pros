f7cd896fb41bf2237165e1b66ad444fd18cefca0
Alan Wernick <awernick@miners.utep.edu>
2014-12-13 01:49:51 -0700

    Scrap SessionsController Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,27 @@ class ApplicationController < ActionController::Base
   	if session[:user_id]
       @current_user = User.find(session[:user_id])
     else
-      @current_user = create_session_path
+      @current_user = create_session
     end
     return @current_user
   end
 
+  def create_session
+    user = User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
+    if user
+      session[:user_id] = user.id
+    end
+    return user
+  end
+
+  def session_destroy
+    utep_cookie = cookies[:UTEP_SE]
+    session[:user_id] = nil
+    client = Savon.client(wsdl: 'http://websvs.utep.edu/databaseservices/public/ExternalSignon.asmx?wsdl')
+    client.call(:log_off, message: { sessionId: utep_cookie.to_s})
+    redirect_to root_url
+  end
+
+  helper_method :session_destroy
   helper_method :current_user
 end
