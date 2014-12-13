8b71e9c58bc641f844a0028495c336b32ac6d59e
Alan Wernick <awernick@miners.utep.edu>
2014-12-13 01:13:26 -0700

    Refactor SSO Auth call to SessionsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,10 @@ class ApplicationController < ActionController::Base
   	if session[:user_id]
       @curret_user = User.find(session[:user_id])
     else
-      @current_user = User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
-        if @current_user
-          session[:user_id] = @current_user.id
-        end
-      return @current_user
+      @curret_user = create_session_path
     end
+    return @current_user
   end
 
-
-	def sso_signout
-		utep_cookie = cookies[:UTEP_SE]
-		session[:user_id] = nil
-		client = Savon.client(wsdl: 'http://websvs.utep.edu/databaseservices/public/ExternalSignon.asmx?wsdl')
-		client.call(:log_off, message: { sessionId: utep_cookie.to_s})
-	end
-
-	helper_method :sso_signout
   helper_method :current_user
 end
