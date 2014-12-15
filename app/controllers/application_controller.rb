c920823111f7b597bd1dc455abb7d2edd7ecddbb
Alan Wernick <awernick@miners.utep.edu>
2014-12-15 00:46:25 -0700

    Fix Navbar's CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,7 @@ class ApplicationController < ActionController::Base
   def current_user
   	if session[:user_id]
       @current_user = User.find(session[:user_id])
-    else
-      @current_user = create_session_path
     end
-    return @current_user
   end
 
   helper_method :current_user
