923153e6f8cbe9af06f052b57abdf6bf80136999
Guillermo Vargas <gavargas22@gmail.com>
2014-12-16 19:44:23 -0700

    Added stuff for the schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141211180814) do
     t.string   "metakeywords"
     t.string   "metadescription"
     t.boolean  "recurring"
+    t.boolean  "repeat_also"
     t.boolean  "registration"
     t.boolean  "comments"
     t.string   "icon"
