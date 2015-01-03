e0c0a52a3cdc2c80dd769e094b695452ce8263a0
Guillermo Vargas <gavargas22@gmail.com>
2015-01-02 18:04:32 -0700

    Latest backend and some fronted functionality

--- app/helpers/event_date_helper.rb
+++ app/helpers/event_date_helper.rb
@@ -19,6 +19,7 @@ module EventDateHelper
 		parsed_date.strftime('%Y-%m-%d')
 	end
 
+	#Functions for formatting the time elapsed since event occurred, this is for the notifications.
 	def activity_format_date (span)
 		time = Time.at(span)
 
