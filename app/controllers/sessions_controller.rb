5d6d8784ceb1bb6434236377e2fae27a24feebc9
Alan Wernick <awernick@miners.utep.edu>
2015-02-05 00:31:16 -0700

    Refactor SSO code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
 	#TODO: Return to previous page
 	def create
 		user = User.from_sso(cookies[:UTEP_SE], cookies[:UTEP_SA])
-		session[:user_id] = user.id if user
+		log_in(user) if user
 		redirect_to root_url
 	end
 
 	def destroy
-		session[:user_id] = nil
+		log_out
 		UTEPSSO.deauthenticate(cookies[:UTEP_SE], cookies[:UTEP_SA])
 		redirect_to root_url
 	end
